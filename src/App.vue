50d1d67ca7fbcb928c0382f44c7fca3c66a52d88
Sarath Kumar <03sarath@gmail.com>
2024-08-03 22:21:26 +0530

    fixed AB#45

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen Ai for Psitron Technologies</h1>
+    <h1>Gen Ai for All Business</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"

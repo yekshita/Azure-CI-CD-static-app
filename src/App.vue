cce87182f2a13ef02619fe96c976b9529ada36df
Srinivas <nihalrao7093@gmail.com>
2025-09-27 22:45:31 +0530

    fixed AB#2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Welcome to Psitron world</h1>
+    <h1>Welcome to AI world</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"

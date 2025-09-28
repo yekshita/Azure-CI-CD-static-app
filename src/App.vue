b4113332e63948079b0140193399fc458a022c95
Srinivas <nihalrao7093@gmail.com>
2025-09-28 20:21:18 +0530

    fixed AB#4

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <h1>Welcome to AGI World</h1>
-	<h2>Upload your data to get Gen Ai predictions</h2>
+	<h2>Upload your data to get Gen ASI predictions</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
